68d0a040ca65bba9f9e688ea131f59ee9d01ab6b
shirodkar <shirodkar.sachin@gmail.com>
2024-06-11 14:47:35 -0400

    Added port to devfile

--- src/main/java/com/shirodkar/servicemeshdemo/serviceone/rest/ServiceOneResource.java
+++ src/main/java/com/shirodkar/servicemeshdemo/serviceone/rest/ServiceOneResource.java
@@ -1,7 +1,5 @@
 package com.shirodkar.servicemeshdemo.serviceone.rest;
 
-import javax.sound.midi.SysexMessage;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
