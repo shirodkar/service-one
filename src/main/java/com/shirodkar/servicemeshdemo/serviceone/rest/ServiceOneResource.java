26ed619201d1a47284ead4e322c9953dd5693897
shirodkar <shirodkar.sachin@gmail.com>
2024-07-15 10:11:02 +0800

    test pipeline

--- src/main/java/com/shirodkar/servicemeshdemo/serviceone/rest/ServiceOneResource.java
+++ src/main/java/com/shirodkar/servicemeshdemo/serviceone/rest/ServiceOneResource.java
@@ -23,7 +23,7 @@ public class ServiceOneResource {
 
     logger.debug("Service-One received the value - '{}'", value);
 
-    // Pass the value as is to another microservice
+    // Pass the value as is to another service
     RestClient.Builder restClientBuilder = RestClient.builder();
     this.restClient = restClientBuilder.baseUrl(baseUrl).build();
     String response = this.restClient.get().uri("/handle/{value}", value).retrieve().body(String.class);
